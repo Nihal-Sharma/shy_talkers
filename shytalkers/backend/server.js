9241475b6b4895845e4eab8878090f0df1358732
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2023-12-30 21:30:13 +0530

    commit 1

--- shytalkers/backend/server.js
+++ shytalkers/backend/server.js
@@ -11,17 +11,14 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json()); // to accept json data
+app.use(express.json()); 
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -37,9 +34,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// --------------------------deployment------------------------------
 
-// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
